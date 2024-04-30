c43af726dbdb8199e26a171e0a5464a9bc46a5ce
Mohamed Ahmed <mohamed.5580.558@gmail.com>
2024-04-30 23:06:09 +0300

    up

--- Xperienced/Xperienced/settings.py
+++ Xperienced/Xperienced/settings.py
@@ -122,6 +122,11 @@ STATIC_URL = "static/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static/')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'Xperienced/static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
