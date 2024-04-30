289fd5a1eaa653b11c2c7cce2b1c2527d86d1ae1
Sherif Youssef <sherifyousef209@gmail.com>
2024-04-30 15:05:31 +0300

    edited/added some models

--- Xperienced/assistantFinder/models.py
+++ Xperienced/assistantFinder/models.py
@@ -3,15 +3,21 @@ from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from phonenumber_field.modelfields import PhoneNumberField
 
+OPEN = "Open"
+PENDING = "Pending"
+PROGRESS = "In progress"
+COMPLETED = "Completed"
+CANCELLED = "Cancelled"
+
 class User(AbstractUser):
     phone = PhoneNumberField()
 
-class RequestType(models.Model):
-    name = models.CharField(max_length=100)
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    requestType = models.ForeignKey(RequestType, on_delete=models.CASCADE, related_name="categories")
+    requestType = models.CharField(max_length=25, choices=[
+        ("Academic Support", "Academic Support"), 
+        ("Mentorship", "Mentorship")
+        ])
 
 class Request(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="requests")
@@ -19,24 +25,70 @@ class Request(models.Model):
     description = models.TextField(max_length=1000)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="requests")
     budget = models.IntegerField()
-    state = models.CharField(max_length=100, choices=[
-        ("Open", "Open"), 
-        ("In progress", "In progress"), 
-        ("Cancelled", "Cancelled"), 
-        ("Completed", "Completed")
-        ])
+    cancelled = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def state(self):
+        if self.cancelled:
+            return "Cancelled"
+        for offer in self.offers:
+            if offer.state in [PROGRESS, COMPLETED]:
+                return offer.state
+        return OPEN
+
+    def acceptOffer(self, offer):
+        if offer.request != self or self.state() != OPEN:
+            return False
+        offer.state = PROGRESS
+        return True
+
+    def cancelOffer(self, offer):
+        if offer.request != self or offer.state != PENDING:
+            return False
+        offer.state = CANCELLED
+        return True
+
+    def completeRequest(self):
+        if self.state() != PENDING:
+            return False
+        offer = self.offers.get(state=PENDING)
+        offer.state = COMPLETED
+    
+    def cancelRequest(self):
+        if self.state() != OPEN:
+            return FALSE
+        self.cancelled = True
+        
 
 class Offer(models.Model):
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="offers")
     request = models.ForeignKey(Request, on_delete=models.CASCADE, related_name="offers")
     bid = models.IntegerField()
     notes = models.TextField(max_length=200)
+    state = models.CharField(max_length=100, default=PENDING, choices=[
+        (PENDING, PENDING), 
+        (PROGRESS, PROGRESS), 
+        (CANCELLED, CANCELLED), 
+        (COMPLETED, COMPLETED)
+        ])
+    date = models.DateTimeField(auto_now_add=True)
 
 class Connection(models.Model):
     offer = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name="connections")
     date = models.DateTimeField(auto_now_add=True)
 
+    def instructor(self):
+        return self.offer.request.owner
+    def student(self):
+        return self.offer.bidder
+    def isActive(self):
+        return self.offer.state == PROGRESS
+
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
     connection = models.ForeignKey(Connection, on_delete=models.CASCADE, related_name="messages")
-    
+    content = models.TextField(max_length=1000)
+    byInstructor = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def sender(self):
+        return self.connection.instructor() if self.byInstructor else self.connection.student()
