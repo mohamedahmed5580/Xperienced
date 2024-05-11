99fdbf9ecbdd9122927f324dfaee200533b85c94
Mohamed Ahmed <mohamed.5580.558@gmail.com>
2024-05-11 13:45:13 +0300

    .

--- Xperienced/assistantFinder/static/js/signin.js
+++ Xperienced/assistantFinder/static/js/signin.js
@@ -2,26 +2,18 @@ const form = document.getElementById('form');
 
 document.getElementById('form').addEventListener('submit', function(event) {
   event.preventDefault();
-
+  const formData = new FormData(form);
   fetch('/api/login/', {
       method: 'POST',
       headers: {
-          'Content-Type': 'application/json',
+        'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value,
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-          firstname: document.getElementById('firstname').value,
-          lastname: document.getElementById('lastname').value,
-          username: document.getElementById('username').value,
-          email: document.getElementById('email').value,
-          phone: document.getElementById('phone').value,
-          password: document.getElementById('Password').value,
-          confirm_password: document.getElementById('ConfirmPassword').value
-      }),
+      body: JSON.stringify(Object.fromEntries(formData)),
   })
   .then(response => response.json())
   .then(data => {
-      console.log(data); 
-      
+    console.log(data);
   })
 
 });
